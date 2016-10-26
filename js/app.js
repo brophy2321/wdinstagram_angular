57f88f2f6e96a9776c219d831d0c94b29864a133
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-26 13:59:46 -0400

    crud

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-"use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+
+let instaData = [
+  {photo_url: 'http://images.footballfanatics.com/AppLogos/NFL/NFL_Pittsburgh_Steelers_200x200_trans.png', author: 'Ryan ', body: '6X Superbowl Champs '},
+  {photo_url: 'https://pbs.twimg.com/profile_images/779056518616473600/9nGIAN9D.jpg', author: 'Ryan', body: '4x Stanley Cup Champs'},
+  {photo_url: ' http://a.fssta.com/content/dam/fsdigital/fscom/global/dev/static_resources/mlb/teams/retina/23.vresize.200.200.medium.0.png', author: 'Ryan', body: '5x World Series Champs'},
+  {photo_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Pitt_Panthers_wordmark.svg/200px-Pitt_Panthers_wordmark.svg.png', author: 'Ryan', body: '9 national titles'}
+]
+
+
+  angular
+  .module("wdinstagram", [])
+  .controller("instaCtrl", [instaController])
+
+
+function instaController(){
+  this.insta = instaData
+  this.newInsta= {}
+    this.addInsta = function () {
+      this.insta.push(this.newInsta)
+      this.newInsta = {}
+    }
+}
