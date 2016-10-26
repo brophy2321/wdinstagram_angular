3ea5d6dcb5c12e760349c6dd90e0e41dbcf11cb3
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-26 18:01:21 -0400

    routerfunction

--- js/app.js
+++ js/app.js
@@ -1,23 +1,63 @@
+"use strict";
 
 
-let wdinstagramData = [
-  {photo_url: 'http://images.footballfanatics.com/AppLogos/NFL/NFL_Pittsburgh_Steelers_200x200_trans.png', author: 'Ryan ', body: '6X Superbowl Champs '},
-  {photo_url: 'https://pbs.twimg.com/profile_images/779056518616473600/9nGIAN9D.jpg', author: 'Ryan', body: '4x Stanley Cup Champs'},
-  {photo_url: 'http://a.fssta.com/content/dam/fsdigital/fscom/global/dev/static_resources/mlb/teams/retina/23.vresize.200.200.medium.0.png', author: 'Ryan', body: '5x World Series Champs'},
-  {photo_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Pitt_Panthers_wordmark.svg/200px-Pitt_Panthers_wordmark.svg.png', author: 'Ryan', body: '9 national titles'}
-]
 
-
+(function(){
   angular
-    .module("wdinstagramApp", ["ui.router"])
-  .controller("wdinstagramCtrl", [ wdinstagramController])
+    .module("wdinstagramApp", [
+      "ui.router",
+      "ngResource"
+    ])
+
+    .config([
+      "$stateProvider" ,
+      RouterFunction
+    ])
+
+    .factory ("InstaFactory", [
+      "$resource",
+      InstaFactoryFunction
+    ])
+    .controller("InstaIndexController" ,[
+      "InstaFactory",
+      InstaIndexControllerFunction
+    ])
+
+    .controller("InstaShowController", [
+      "InstaFactory" ,
+      "$stateParams",
+      InstaShowControllerFunction
+    ])
+  function InstaShowControllerFunction( InstaFactory, $stateParams) {
+    this.entries = InstaFactory.get ( { id: $stateParams.id});
+  }
+
+  function InstaFactoryFunction ($resource) {
+    return $resource ( "http://localhost:3000/entries/:id");
+  }
+  function InstaIndexControllerFunction ( InstaFactory) {
+    this.entries = InstaFactory.query();
+  }
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("entryIndex", {
+      url: "/entries",
+      templateUrl: "js/ng-views/index.html",
+      controller: "InstaIndexController",
+      controllerAs: "InstaIndexViewModel"
+    })
+
+    .state(entryShow)
+  }
 
 
-function wdinstagramController(){
-  this.wdinstagram = wdinstagramData
-  this.newWdinstagram = {}
-    this.addWdinstagram= function () {
-      this.wdinstagram.push(this.newWdinstagram)
-      this.newWdinstagram = {}
-    }
-}
+// function wdinstagramController(){
+//   this.wdinstagram = wdinstagramData
+//   this.newWdinstagram = {}
+//     this.addWdinstagram= function () {
+//       this.wdinstagram.push(this.newWdinstagram)
+//       this.newWdinstagram = {}
+//     }
+// }
+}());
